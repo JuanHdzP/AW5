e9420d467f134300e02370b2de12463fbae7a5d8
Juan Hernandez Puebla <al222010053@gmail.com>
2022-03-19 13:17:41 -0600

    Cambios

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -1,16 +1,12 @@
 <?php
-
 namespace App\Http\Controllers\Api;
-
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Notifications\SignupActivate;
 use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Validator;
-
 class RegisterController extends ApiController
 {
-
 public function register(Request $request)
 {
 $validator = Validator::make($request->all(), [
@@ -31,7 +27,6 @@ $validator = Validator::make($request->all(), [
 . User::ADMINISTRATION ,
 'partner' => 'required|bool' */
 ]);
-
 /* if ($request->hasFile('img_perfil')) {
 $rules = [
 'img_perfil' => 'mimes:jpg,jpeg,png|max:2048',
@@ -43,8 +38,6 @@ $this->validate($request, $rules);
 if ($validator->fails()) {
 return $this->errorResponse($validator->errors(), 401);
 }
-
-
 $user = new User([
 'name' => $request->name,
 'email' => $request->email,
@@ -52,16 +45,13 @@ $user = new User([
 'telefono' => $request->telefono,
 'password' => bcrypt($request->password),
 'type_user_id'=> $request->type_user_id,
-
 ]);
-
 /* if ($request->hasFile('img_perfil')) {
 $image = file_get_contents($request->file('img_perfil')->path());
 $base64Image = base64_encode($image);
 $user->image = $this->saveImages($base64Image, 'users', $user->id);
 } */
 $user->save();
-
 /* $user->notify(new SignupActivate($user));
  */
 return response()->json(['message' => 'Por favor, confirme haciendo clic en el botón verificar usuario que se le envió en su correo electrónico'], 201);
